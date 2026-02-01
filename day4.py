1a866a18426c2e3e1bca5b95b4ef7c9b8d4f4389
Sonal Tiwari <sonalbca23@gmail.com>
2026-02-01 10:09:41 +0530

    initial commit

---
+++ day4.py
@@ -0,0 +1,6 @@
+l = int(input("Enter the length:- "))
+b = int(input("Enter the breadth:- "))
+area = l*b
+perimeter = 2*(l+b)
+print("Area of rectangle:- ",area)
+print("Perimeter of rectangle:- ",perimeter)
