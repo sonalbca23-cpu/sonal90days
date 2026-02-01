a747234e80210482dc25c8e86c9c5b1ade2b2ae5
Sonal Tiwari <sonalbca23@gmail.com>
2026-02-01 10:08:47 +0530

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,5 +1,6 @@
 r = float(input("Enter the Radius:- "))
 area = 3.14*r*r
-parameter = 2*3.14*r
+perimeter = 2*3.14*r
 print("Area of circle:- ",area)
-print("Parameter of circle:- ",parameter)
+
+print("Perimeter of circle:- ",perimeter)
